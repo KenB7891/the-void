e0905cc773a59c72058d245009b58682cf916cd4
Kenneth Bilicke <kbilicke7891@gmail.com>
2025-05-10 03:18:11 -0400

    Fixed db selection based on environment. Updated .env file for production and overwrite for testing in ci.yml.

--- app/database.py
+++ app/database.py
@@ -5,16 +5,23 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DB_HOST = os.getenv('DB_HOST')
-DB_PORT = os.getenv('DB_PORT', '5432')
-DB_NAME = os.getenv('DB_NAME')
-DB_USER = os.getenv('DB_USER')
-DB_PASSWORD = os.getenv('DB_PASSWORD')
+ENVIRONMENT = os.getenv('ENVIRONMENT', 'PROD')
 
-DATABASE_URL = f'postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
+if ENVIRONMENT == 'TEST':
+    engine = create_engine('sqlite:///:memory:', connect_args={'check_same_thread': False})
+    SessionLocal = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
 
-engine = create_engine(DATABASE_URL)
-SessionLocal = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
+else:
+    DB_HOST = os.getenv('DB_HOST')
+    DB_PORT = os.getenv('DB_PORT', '5432')
+    DB_NAME = os.getenv('DB_NAME')
+    DB_USER = os.getenv('DB_USER')
+    DB_PASSWORD = os.getenv('DB_PASSWORD')
+
+    DATABASE_URL = f'postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
+    engine = create_engine(DATABASE_URL)
+    SessionLocal = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))    
+
 
 Base = declarative_base()
 
